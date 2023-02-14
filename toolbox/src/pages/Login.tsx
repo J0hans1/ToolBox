527bf68170f5b3b1ffc81a235bd9c8f88334c8b2
Hans Kristian Bjørgo Kværum <hkkvaeru@stud.ntnu.no>
2023-02-14 15:20:01 +0100

    #39 Refactoring: Login Page

--- toolbox/src/pages/Login.tsx
+++ toolbox/src/pages/Login.tsx
@@ -3,6 +3,8 @@ import PEOPLE from '../img/people.svg';
 import { useNavigate } from "react-router-dom";
 import { removeFromSessionStorage, validateUser } from '../lib/controller';
 import { useState } from 'react';
+import Step from '../components/Step';
+import Title from '../components/Title';
 
 
 // Check for valid input on Login
@@ -22,7 +24,7 @@ async function validation(username: string, password: string) {
         return true;
     } else {
         alert("Brukernavn eller passord er feil");
-        return false; 
+        return false;
     }
 }
 
@@ -44,44 +46,68 @@ const LoginPage = () => {
     let navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    
-    return(
-        <div className="h-screen" >
-            <div className='flex h-full'>
-                <div className='flex flex-col justify-between w-1/4 bg-yellow-400 h-full'></div>
-                <div className='flex flex-col w-2/4 p-6 pt-40' >
-                    <div>
-                        <img src={PEOPLE} className='h-10' alt="People icon"/>
+
+    return (
+        <div className="w-screen h-screen" >
+
+            <div id="c_section" className='flex w-full h-full content-center bg-slate-100 overflow-hidden z-10'>
+                <div id="c_container" className='static flex flex-row h-screen m-auto w-full max-w-7xl p-10 gap-10 justify-center bg-white'>
+                    {/* <div className='flex flex-col justify-between w-1/4 bg-yellow-400 h-full'></div> */}
+                    <div className='flex flex-col w-10/12 text-left pt-40 mb-10'>
+                        <div>
+                            <img src={PEOPLE} className='h-10' alt="People icon" />
+                        </div>
+
+                        <Title size={'text-7xl'} heading={'Logg '} span={'inn'} description={''} />
+
+
+                        <div id="LOGIN_USER" className='flex flex-col my-5'>
+                            <div className='flex flex-col w-full gap-2'>
+                                <TextField color="primary" label="Brukernavn" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
+                                <TextField color="primary" label="Passord" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                                <Button
+                                    variant="contained"
+                                    sx={{ p: 2 }}
+                                    onClick={async () => {
+                                        if (await validation(username, password)) {
+                                            alert("Du er nå logget inn på bruker: " + username);
+                                            navigate("/");  /* navigerer fra loginpage til hovedsiden */
+                                        }
+                                    }}>Logg inn
+                                </Button>
+
+                                <Button
+                                    color="info"
+                                    variant="contained"
+                                    sx={{ p: 2 }}
+                                    href="/#/Register">Registrer bruker
+                                </Button>
+
+
+
+                                <Button
+                                    variant="outlined"
+                                    sx={{ p: 2 }}
+                                    onClick={() => {
+                                        removeFromSessionStorage(); // fjerner brukernavn og brukerID fra session storage
+                                        console.log("Logged out");
+                                        alert("Du er nå logget ut");
+                                    }}>Logg ut
+                                </Button>
+                            </div>
+                        </div>
+
+
+
                     </div>
-                    <Header/>
-                    <div className='flex flex-col h-3/4'>
-                        <div className='flex justify-between h-32 flex-col'>
-                            <TextField color="primary" label="Brukernavn" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
-                            <TextField color="primary" label="Passord" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)}/>                              
-                        </div>
-                        
-                        <div className='mt-10'>   
-                            <Button 
-                            color="primary" 
-                            variant="contained" 
-                            onClick={async () => {
-                                if (await validation(username, password)) {
-                                    alert("Du er nå logget inn på bruker: " + username);
-                                    navigate("/");  /* navigerer fra loginpage til hovedsiden */
-                            }}}>Login</Button>
-                            <Button 
-                            color="primary" 
-                            variant="contained" 
-                            onClick={() => {
-                                removeFromSessionStorage(); // fjerner brukernavn og brukerID fra session storage
-                                console.log("Logged out");
-                                alert("Du er nå logget ut");
-                                }}>Logout</Button>
-                        </div>
-                    </div>  
                 </div>
+
             </div>
+
         </div>
+
+
     );
 }
 
