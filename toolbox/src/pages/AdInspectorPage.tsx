34b6afcb25d71ce7bf58e29724867a759eb8543c
Anne Olderheim <annelold@stud.ntnu.no>
2023-03-07 14:17:44 +0100

    #75 make map API
    Add map API for ad and get directions
    
    Relates: #75

--- toolbox/src/pages/AdInspectorPage.tsx
+++ toolbox/src/pages/AdInspectorPage.tsx
@@ -43,6 +43,8 @@ const AdUserInfo = (props: Info) => {
 
             </div >
 
+
+
         </div>
     )
 }
@@ -262,7 +264,22 @@ const AdInspectorPage = () => {
                                     <TitledIcon icon="https://img.icons8.com/ios/50/000000/price-tag-euro.png" key={ad.price} text={ad.price?.toString() + " kr/dag"} iconSize="h-full" textSize="text-3xl" />
                                     <TitledIcon icon="https://img.icons8.com/material-sharp/256/map-marker.png" key={ad.city} text={ad.city} iconSize="h-full" textSize="text-3xl" />
                                 </div>
+                                
+                                <div className="flex flex-col">
+                                    <div className="bg-white text-3xl">{`${ad.address} ${ad.zip} ${ad.city}`}</div>
+                                    <a href={`https://www.google.com/maps/dir/?api=1&destination=${ad.address} ${ad.zip} ${ad.city}`}>
+                                        <div className="absolute pt-40 text-center pl-7">
+                                            Trykk her for veibeskrivelse
+
+                                        </div>
+                                        <div className="opacity-50 flex relative">
+                                            <Map address={`${ad.address} ${ad.zip} ${ad.city}`} />
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
+
+
 
                         )}
 
