718b5ea627ee3910134c27b60108ab37d8b9586c
Martin Isaksen <martin.isaksen@live.no>
2023-03-21 13:13:39 +0100

    #101 fix avatar on adspage

--- toolbox/src/components/Ad.tsx
+++ toolbox/src/components/Ad.tsx
@@ -2,21 +2,22 @@ import { Avatar } from "@mui/material";
 import { amber } from "@mui/material/colors";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Ad } from "../types/types";
+import { Ad, GoogleUser } from "../types/types";
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import RoomIcon from '@mui/icons-material/Room';
+import { getUser } from "../lib/controller";
 
 
 interface AdProps {
     ad: Ad;
 }
 
-export default function AdComponent({ad}: AdProps){
+export default function AdComponent({ ad }: AdProps) {
 
     let navigate = useNavigate();
     const [picture, setPicture] = useState<string>("");
 
-    function truncate(str: any ) {
+    function truncate(str: any) {
         return (str.length > 30) ? str.substring(0, 30 - 1) + '...' : str;
     };
 
@@ -36,60 +37,69 @@ export default function AdComponent({ad}: AdProps){
             return "https://img.icons8.com/ios/50/000000/price-tag-euro.png";
         }
     }
-    
+
+    const [image, setImage] = useState<string>("https://img.icons8.com/ios/50/000000/price-tag-euro.png");
+
+    async function getUserFromDatabase(id: string) {
+        const user = await getUser(id);
+        if (user === undefined || user === null) return undefined;
+        const userFromDatabase = { id: user.id, ...user.data() } as GoogleUser;
+        if (userFromDatabase.photoURL !== undefined && userFromDatabase.photoURL !== null) {
+            setImage(userFromDatabase.photoURL);
+        }
+    }
 
     useEffect(() => {
         getAdPicture();
-    }, [])
+        if (ad.userid !== undefined) {
+            getUserFromDatabase(ad.userid);
+        }
+    }, []);
 
 
     return (
-        <div 
-            onClick={() => handleClick()} 
+        <div
+            onClick={() => handleClick()}
             className='
                 rounded-lg w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 
                 shadow-lg overflow-hidden relative hover:scale-105 hover:shadow-2xl dark:shadow-dark-white/25 active:scale-100 duration-200 m-3
             '>
-            <div className="flex h-full w-full overflow-hidden bg-cover bg-center" style={{backgroundImage: `url(${picture})`}}>
-                {/*
-                !Denne linjen med kode legger et bilde over bakgrunnsbildet, det er det som skaper gjentagelses buggen. 
-                <img className="h-40 w-full " src={picture} alt="AdPicture" /> med h-full vil deler av bildet bli dekket av tekst
-                */}
+            <div className="flex h-full w-full overflow-hidden bg-cover bg-center" style={{ backgroundImage: `url(${picture})` }}>
             </div>
 
-        <div className="flex flex-row">
+            <div className="flex flex-row">
 
-        <div className="flex flex-row bg-white dark:bg-dark-lysGraa dark:text-dark-white rounded-lg h-auto absolute bottom-0 p-4 justify-between w-full shadow-inner ">
+                <div className="flex flex-row bg-white dark:bg-dark-lysGraa dark:text-dark-white rounded-lg h-auto absolute bottom-0 p-4 justify-between w-full shadow-inner ">
 
-                <div className="w-1/5 hidden md:block">
-                    <Avatar sx={{bgcolor: amber[500]}}>TH</Avatar>
-                </div>
-                <div className="flex flex-col w-4/5">
-                    <div className="flex justify-between">
-                        <h2 className="text-md font-bold">{ad.title}</h2>
-
-                        <div className="flex flex-row h-5 lg:hidden">
-                            <img alt="bilde" className="h-full" src="https://img.icons8.com/ios/50/000000/price-tag-euro.png" />
-                            <p className="md:text-lg text-pu-ghost ml-1 dark:text-dark-white">{ad.price}kr/dag</p>
-                        </div>
-
+                    <div className="w-1/5 hidden md:block">
+                        <img src={image} alt="User profile" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }} />
                     </div>
-                    <p className="text-sm text-left my-2 hidden lg:block">{truncate(ad.description)}</p>
-                    <div className=" flex-row gap-5 hidden lg:flex">
+                    <div className="flex flex-col w-4/5">
+                        <div className="flex justify-between">
+                            <h2 className="text-md font-bold">{ad.title}</h2>
 
-                        <div className="flex flex-row h-5">
-                            <AttachMoneyIcon color="secondary"/>
-                            <p className="text-md text-pu-ghost ml-1 dark:text-dark-white">{ad.price}kr</p>
+                            <div className="flex flex-row h-5 lg:hidden">
+                                <img alt="bilde" className="h-full" src="https://img.icons8.com/ios/50/000000/price-tag-euro.png" />
+                                <p className="md:text-lg text-pu-ghost ml-1 dark:text-dark-white">{ad.price}kr/dag</p>
+                            </div>
+
                         </div>
-                        
-                        <div className="flex flex-row h-5">
-                            <RoomIcon color="secondary"/>
-                            <p className="text-md text-pu-ghost ml-1 dark:text-dark-white">{ad.city}</p>
+                        <p className="text-sm text-left my-2 hidden lg:block">{truncate(ad.description)}</p>
+                        <div className=" flex-row gap-5 hidden lg:flex">
+
+                            <div className="flex flex-row h-5">
+                                <AttachMoneyIcon color="secondary" />
+                                <p className="text-md text-pu-ghost ml-1 dark:text-dark-white">{ad.price}kr</p>
+                            </div>
+
+                            <div className="flex flex-row h-5">
+                                <RoomIcon color="secondary" />
+                                <p className="text-md text-pu-ghost ml-1 dark:text-dark-white">{ad.city}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
-        </div>
-    </div>
-        )
+    )
 }
